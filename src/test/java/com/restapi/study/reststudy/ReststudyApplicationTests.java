5c5155408752d4aa8b20eb51587cf9689dbe2c4a
rheew <47098896+rheew@users.noreply.github.com>
2021-01-01 12:24:04 +0900

    Initial commit

---
+++ src/test/java/com/restapi/study/reststudy/ReststudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.study.reststudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReststudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
