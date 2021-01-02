3bfdc6f111de143eeb685ec9816bec407dee31e1
rheew <47098896+rheew@users.noreply.github.com>
2021-01-02 11:12:38 +0900

    POST /api/events returns 201

--- src/main/java/com/restapi/study/reststudy/events/Event.java
+++ src/main/java/com/restapi/study/reststudy/events/Event.java
@@ -2,17 +2,21 @@ package com.restapi.study.reststudy.events;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Builder @AllArgsConstructor @NoArgsConstructor
 @Getter @Setter @EqualsAndHashCode(of = "id")
+@Entity
 public class Event {
 
+    @Id @GeneratedValue
     private Integer id;
-    private String name; private String description; private
-    LocalDateTime beginEnrollmentDateTime; private
-    LocalDateTime closeEnrollmentDateTime; private
-    LocalDateTime beginEventDateTime;
+    private String name;
+    private String description;
+    private LocalDateTime beginEnrollmentDateTime;
+    private LocalDateTime closeEnrollmentDateTime;
+    private LocalDateTime beginEventDateTime;
     private LocalDateTime endEventDateTime;
     private String location; // (optional)이게 없으면 온라인 모임
     private int basePrice; // (optional)
@@ -20,5 +24,6 @@ public class Event {
     private int limitOfEnrollment;
     private boolean offline;
     private boolean free;
-    private EventStatus eventStatus = EventStatus.DRAFT;
+    @Enumerated(EnumType.STRING)
+    private EventStatus eventStatus;
 }
